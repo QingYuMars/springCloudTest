a009bccfa3f2447fb49683c25be5ff91d34c3604
QingYuMars <wechat987654321@163.com>
2021-04-13 17:09:08 +0800

    springcloud添加gateway

--- web/src/main/java/com/cloud/web/controller/TestWebController.java
+++ web/src/main/java/com/cloud/web/controller/TestWebController.java
@@ -2,8 +2,6 @@ package com.cloud.web.controller;
 
 import com.cloud.server.apis.TestServicesApis;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
