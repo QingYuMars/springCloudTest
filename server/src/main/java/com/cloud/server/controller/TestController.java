b742457f28e276068b335de4e193489e755f7b10
QingYuMars <wechat987654321@163.com>
2021-04-14 14:26:50 +0800

    spring cloud config

--- server/src/main/java/com/cloud/server/controller/TestController.java
+++ server/src/main/java/com/cloud/server/controller/TestController.java
@@ -2,6 +2,7 @@ package com.cloud.server.controller;
 
 import com.cloud.server.service.TestServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +14,16 @@ public class TestController {
     @Autowired
     private TestServices testServices;
 
+    @Value("${aa.bb}")
+    String value;
+
     @RequestMapping("/get")
     public String get(@RequestParam("name") String name) {
         return testServices.get(name);
+    }
+
+    @RequestMapping("/values")
+    public String value(){
+        return "value:" + value;
     }
 }
--- web/src/main/java/com/cloud/web/controller/TestWebController.java
+++ web/src/main/java/com/cloud/web/controller/TestWebController.java
@@ -2,6 +2,7 @@ package com.cloud.web.controller;
 
 import com.cloud.server.apis.TestServicesApis;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,9 @@ public class TestWebController {
     @Autowired
     private TestServicesApis testServicesApis;
 
+    @Value("${aa.bb}")
+    String value;
+
     @RequestMapping("/getTest")
     public String getTest() {
         return testServicesApis.get("通过web调用的");
@@ -20,6 +24,11 @@ public class TestWebController {
     @RequestMapping("/hello")
     public String getHello(){
         return "Hello";
+    }
+
+    @RequestMapping("/value")
+    public String value(){
+        return "value:" + value;
     }
 
 }
